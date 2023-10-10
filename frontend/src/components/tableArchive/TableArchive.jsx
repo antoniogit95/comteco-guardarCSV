f603aea88cf27f4da4e268d3e773593a556d20d4
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-10-10 11:16:44 -0400

    agregar un buscador por nombre del cliente

--- frontend/src/components/tableArchive/TableArchive.jsx
+++ frontend/src/components/tableArchive/TableArchive.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './TableArchive.css'
 
 export const  TableArchive = () => {
     const [datos, setDatos] = useState([]);
+    const [rescatarDatos, setRescatarDatos] = useState([])
+    const [select, setSelectd] = useState("");
+    const [buscar, setBuscar] = useState("");
 
     useEffect( () => {
         getAllDatos();
@@ -12,6 +16,7 @@ export const  TableArchive = () => {
         try {
             const response = await axios.get('http://localhost:8080/api/v1/files/get');
             setDatos(response.data);
+            setRescatarDatos(response.data);
             console.log("datos rescatados exitosamente")
         } catch (error) {
             console.log('Error al obtener datos:', error);
@@ -19,9 +24,60 @@ export const  TableArchive = () => {
         
     }
 
+    const filtrar = (terminoBusqueda, busquedaPor) => {
+        console.log(terminoBusqueda +" - "+ busquedaPor + "mostrando algo")
+        if(terminoBusqueda !== ''){
+            var resultadoBusqueda = rescatarDatos.filter((elemento) => {
+                console.log(elemento.cliente.toString + "imprimir algo de prueba")
+                if(elemento.cliente.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())){
+                    return elemento;
+                }
+            });
+            setDatos(resultadoBusqueda);
+        }
+    }
+    
     return (
         <div>
             <h1>Lista de Datos</h1>
+            <input 
+                type='text'
+                id='buscar'
+                name='buscar'
+                placeholder='escribe tu elemento de busqueda'
+                value={buscar}
+                onChange={ (e) => setBuscar(e.target.value) }
+            ></input>
+            <select 
+                value={select}
+                onChange={(e) => setSelectd(e.target.value)}
+                >
+                <option value = '' >seleccione un elemnto</option>
+                <option value = 'fecha_creacion' >FECHA_CREACION</option>
+                <option value = 'cod_tipo_sol'>COD_TIPO_SOL</option>
+                <option value = 'tipo_solicitud'>TIPO_SOLICITUD</option>
+                <option value = 'cod_tipo'>COD_TIPO</option>
+                <option value = 'tipo_trabajo'>TIPO_TRABAJO</option>
+                <option value = 'cod_plan_comercial'>COD_PLAN_COMERCIAL</option>
+                <option value = 'plan_comercial'>PLAN_COMERCIAL</option>
+                <option value = 'componente'>COMPONENTE</option>
+                <option value = 'clase_servicio'>CLASE_SERVICIO</option>
+                <option value = 'area_servicio'>AREA_SERVICIO</option>
+                <option value = 'ubicacion'>UBICACION</option>
+                <option value = 'producto'>PRODUCTO</option>
+                <option value = 'orden'>ORDEN</option>
+                <option value = 'desc_servicio'>DESC_SERVICIO</option>
+                <option value = 'numero_servicio'>NUMERO_SERVICIO</option>
+                <option value = 'estado_componente'>ESTADO_COMPONENTE</option>
+                <option value = 'nap'>NAP</option>
+                <option value = 'posicion'>POSICION</option>
+                <option value = 'descripcion'>DESCRIPCION</option>
+                <option value = 'cliente'>CLIENTE</option>
+                <option value = 'direccion'>DIRECCION</option>
+                <option value = 'cod_estado_ot'>COD_ESTADO_OT</option>
+                <option value = 'unidad_operativa'>UNIDAD_OPERATIVA</option>
+            </select>
+            <button onClick={ () => filtrar(buscar, select)}>Buscar</button>
             <table>
                 <thead>
                     <tr>
